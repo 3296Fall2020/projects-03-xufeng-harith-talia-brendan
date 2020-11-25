b101af9253c7e8e2984bef187143305159b135b4
tug84792 <47865723+tug84792@users.noreply.github.com>
2020-11-25 14:08:47 -0600

    Added functionality to attach pdf and image

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import smtplib
 import os
+import smtplib
 from email.message import EmailMessage
 import imghdr
+from email.mime.text import MIMEText
+
+from fpdf import FPDF
 
 EMAIL_USER = "knockknockcis3296@gmail.com"
 EMAIL_PASSWORD = "Cis3296!"
@@ -12,13 +15,58 @@ msg['From'] = EMAIL_USER
 msg['To'] = 'tug84792@temple.edu'
 msg.set_content('A package has been delivered at your door from (EXACT TYPE OF COURIER)')
 
-with open('test.jpg','rb') as f:
-	file_data = f.read()
-	file_type = imghdr.what(f.name)
-	file_name = f.name
-	#print(file_type)
+#Section to convert a text file to a pdf
+#======================================================================#
+def txtToPDF(txtFile):
 
-msg.add_attachment(file_data, maintype='image', subtype=file_type, filename = file_name)
+	pdf = FPDF()
+	pdf.add_page()
+	pdf.set_font("Arial", size = 10)
+
+	f = open(txtFile, "r")
+	for i in f:
+		pdf.cell(0,txt = i, ln = 1)
+
+	return pdf.output("sampleFile.pdf")
+
+txtFile= "/Users/harith.siddiqui754/PycharmProjects/kkEmail/sampleFile.txt"
+txtToPDF(txtFile)
+#=======================================================================#
+
+#Section to send an email
+#=======================================================================#
+
+#This section attaches an image to the email
+def attachImage(filePath):
+	with open(filePath, 'rb') as f:
+		file_data = f.read()
+		file_type = imghdr.what(f.name)
+		file_name = f.name
+	msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)
+
+#Attaches the pdf
+def attachPDF(filePath):
+	with open(filePath,'rb') as f:
+		file_data = f.read()
+		file_name = f.name
+	msg.add_attachment(file_data, maintype='application', subtype='octet-stream', filename = file_name)
+
+
+"""# Create an object of sendpdf function  
+k = sendpdf(sender_email_address,  
+            receiver_email_address, 
+            sender_email_password, 
+            subject_of_email, 
+            body_of_email, 
+            filename, 
+            location_of_file) 
+  
+# sending an email 
+k.email_send()"""
+#msg.attach(MIMEText(open("sampleFile.pdf").read()))
+# with open('test.jpg','rb') as f:
+# 	file_data = f.read()
+# msg.add_attachment(file_data)
 
 # with open('test.jpg','rb') as f:
 # 	file_data2 = f.read()
@@ -27,7 +75,10 @@ msg.add_attachment(file_data, maintype='image', subtype=file_type, filename = fi
 # 	#print(file_type)
 #
 # msg.add_attachment(file_data2, maintype='image', subtype=file_type2, filename = file_name2)
+attachImage('test.jpg')
+attachPDF('sampleFile.pdf')
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
 	smtp.login(EMAIL_USER, EMAIL_PASSWORD)
 	smtp.send_message(msg)
+#=======================================================================#
