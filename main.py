ccc17bb3afeab1473bf7a18a4d548717c4a1ed6c
XufengRen <59923595+XufengRen@users.noreply.github.com>
2020-12-04 00:47:36 -0500

    Update main.py
    update draw box function:
    - fix text out of the picture
    - color link to the company logo

--- main.py
+++ main.py
@@ -31,17 +31,17 @@ msg['To'] = emailAddress
 
 #Section to convert a text file to a pdf
 #======================================================================#
-def txtToPDF(filePath):
+# def txtToPDF(filePath):
 
-	pdf = FPDF()
-	pdf.add_page()
-	pdf.set_font("Arial", size = 10)
+# 	pdf = FPDF()
+# 	pdf.add_page()
+# 	pdf.set_font("Arial", size = 10)
 
-	f = open(filePath, "r")
-	for i in f:
-		pdf.cell(0,txt = i, ln = 2)
+# 	f = open(filePath, "r")
+# 	for i in f:
+# 		pdf.cell(0,txt = i, ln = 2)
 
-	return pdf.output("output.pdf")
+# 	return pdf.output("./results/output.pdf")
 
 #=======================================================================#
 
@@ -67,13 +67,13 @@ def attachPDF(filePath):
     msg.add_attachment(file_data,maintype='application',subtype='octet-stream',filename=file_name)
 
 #Method to send email with attachments as parameters
-def sendAttachments(imgPath, pdfPath,label,confidence):
+def sendAttachments(imgPath, logFilePath,label,confidence):
   if confidence>float(89.9):
     msg.set_content('A '+label+' was detected at ' + current_time+ '. Please check attached text file for more information!')
   else:
     msg.set_content('Looks like a '+label+' stopped by your door at ' + current_time+ '. Please check attached text file for more information!')
   sendIMG(imgPath)
-  sendPDF(pdfPath)
+  sendPDF(logFilePath)
   with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
 	  smtp.login(EMAIL_USER, EMAIL_PASSWORD)
 	  smtp.send_message(msg)
@@ -98,7 +98,7 @@ k.email_send()"""
 ############################
 def logging(label, confidence):
     logfile = "detection_logs/"
-    logfile += datetime.today().strftime('%Y-%m-%d')
+    logfile += datetime.today().strftime('%Y-%m-%d')+".txt"
     txtFile = logfile
     f = open(logfile, "a")
     output = datetime.now().strftime('%H:%M')
@@ -106,7 +106,7 @@ def logging(label, confidence):
         output += "\tDelivery driver detected!\n"
     else:
         output += "\tDelivery driver may have been detected!\n"
-    output += "\t\t{}: {}% confident".format(label, confidence) + "\n\n" 
+    output += "\t{}: {}% confident".format(label, confidence) + "\n\n" 
     f.write(output)
     f.close
     return logfile
@@ -118,45 +118,81 @@ def logging(label, confidence):
 #################################################
 # Drawing box on img according to the detection 
 #################################################
-def convertBack(x, y, w, h):
-    xmin = int(round(x - (w / 2)))
-    xmax = int(round(x + (w / 2)))
-    ymin = int(round(y - (h / 2)))
-    ymax = int(round(y + (h / 2)))
-    return xmin, ymin, xmax, ymax
+def convert_to_coordinate(x, y, w, h):
+    left = int(round(x - (w / 2)))
+    right = int(round(x + (w / 2)))
+    top = int(round(y - (h / 2)))
+    bot = int(round(y + (h / 2)))
+    return left,right,bot,top
 
 
-def cvDrawBoxes(detections, img):
-    # Colored labels dictionary
-    color_dict = {
-        'amazonCourier' : [0, 255, 255], 'fedexCourier': [238, 123, 158], 'ups_courier' : [24, 245, 217], 'uspsCourier' : [224, 119, 227]
-    }
+# def cvDrawBoxes(detections, img):
+#     # Colored labels dictionary
+#     color_dict = {
+#         'amazonCourier' : [0, 255, 255], 'fedexCourier': [238, 123, 158], 'ups_courier' : [24, 245, 217], 'uspsCourier' : [224, 119, 227]
+#     }
     
-    for detection in detections:
-        # print("detections[0]: " + str(detection[0]))
-        # print("detections[1]: " + str(detection[1]))
-        x, y, w, h = detection[2][0],\
-            detection[2][1],\
-            detection[2][2],\
-            detection[2][3]
-        name_tag = str(detection[0])#.decode())
-        for name_key, color_val in color_dict.items():
-            if name_key == name_tag:
-                color = color_val 
-                xmin, ymin, xmax, ymax = convertBack(
-                float(x), float(y), float(w), float(h))
-                pt1 = (xmin, ymin)
-                pt2 = (xmax, ymax)
-                cv2.rectangle(img, pt1, pt2, color, 1)
-                cv2.putText(img,
-                            detection[0] +
-                            " [" + str(detection[1]) + "]",
-                            (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                            color, 2)
-    return img
-
-
-
+#     for detection in detections:
+#         # print("detections[0]: " + str(detection[0]))
+#         # print("detections[1]: " + str(detection[1]))
+#         x, y, w, h = detection[2][0],\
+#             detection[2][1],\
+#             detection[2][2],\
+#             detection[2][3]
+#         name_tag = str(detection[0])#.decode())
+#         for name_key, color_val in color_dict.items():
+#             if name_key == name_tag:
+#                 color = color_val 
+#                 xmin, ymin, xmax, ymax = convertBack(
+#                 float(x), float(y), float(w), float(h))
+#                 pt1 = (xmin, ymin)
+#                 pt2 = (xmax, ymax)
+#                 cv2.rectangle(img, pt1, pt2, color, 1)
+#                 cv2.putText(img,
+#                             detection[0] +
+#                             " [" + str(detection[1]) + "]",
+#                             (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+#                             color, 2)
+#     return img
+def drawBoxes(detections,img):
+  box_color = {
+    'amazonCourier':[0,168,225],'fedexCourier':[75,19,136],'ups_courier':[46,20,6],'uspsCourier':[36,60,141]
+  }
+  text_color = {
+    'amazonCourier':[255,255,255],'fedexCourier':[240,80,15],'ups_courier':[242,178,10],'uspsCourier':[255,255,255]
+  }
+  formal_label = {
+    'amazonCourier':'Amazon courier','fedexCourier':'Fedex courier','ups_courier':'UPS driver','uspsCourier':'USPS Driver'
+  }
+  for detection in detections:
+    label=str(detection[0])
+    frame_color=box_color[label]
+    msg_color=text_color[label]
+    tag = formal_label[label]
+    x, y, w, h = detection[2][0],\
+                 detection[2][1],\
+                 detection[2][2],\
+                 detection[2][3]
+    left,right,bot,top = convert_to_coordinate(float(x), float(y), float(w), float(h))
+    pt1=(left,bot)
+    pt2=(right,top)
+    msg = tag+' ' + str(detection[1])+ '%'
+    (text_width, text_height) = cv2.getTextSize(msg, cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.9,thickness=2)[0]
+    cv2.rectangle(img,
+                  pt1,(left+text_width,bot-text_height-10),
+                  frame_color,
+                  cv2.FILLED)
+    cv2.rectangle(img,
+                  pt1,pt2,
+                  frame_color,
+                  2)
+    cv2.putText(img,
+                 msg,
+                 (left,bot - 5), 
+                 cv2.FONT_HERSHEY_SIMPLEX, 
+                 0.9, 
+                 msg_color,2)
+  return img
 ##################################
 #The main part
 ##################################
@@ -217,7 +253,7 @@ def main():
         # detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.5)    # Detection occurs at this line and return detections, for customize we can change the threshold.                                                                                   
         detections = darknet.detect_image(network, class_names, darknet_image, thresh=0.8)
         #print("line 124 pass")
-        image = cvDrawBoxes(detections, frame_resized)               # Call the function cvDrawBoxes() for colored bounding box per class
+        image = drawBoxes(detections, frame_resized)               # Call the function cvDrawBoxes() for colored bounding box per class
         #print("line 126 pass")
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
